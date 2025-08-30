217c181d34fab506612d5941c01c15d974acd0a4
AryanPradhan <Aryanrp2007@gmail.com>
2025-08-30 18:59:39 +0400

    fixed navigation issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
-  site: 'https://AryanPradhan12.github.io/',
+  site: 'https://AryanPradhan12.github.io/PersonalWebsite/',
+  base: '/PersonalWebsite/',
   output: 'static',
 });
